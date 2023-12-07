25eb39bb6d251e7782df6be190d10f87ee7012c1
prayastimalsinaGSA <124933537+prayastimalsinaGSA@users.noreply.github.com>
2023-12-07 13:14:55 -0500

    Update processScan.py

--- scripts/processScan.py
+++ scripts/processScan.py
@@ -1,7 +1,16 @@
 fileName = "./outputHS.json"
 
-# fileName = "/Users/Prayas/Documents/WorkSpace/odp-VulnerableWebApp/action.yml"
+#fileName = "/Users/Prayas/Documents/WorkSpace/odp-VulnerableWebApp/result.json"
 
 f = open(fileName)
-for line in f:
-    print (line)
+data = json.load(f)
+vuln = 0
+sca = 0
+for i in data["analysisVulnerabilities"]:
+    vulnerabliliy = i["vulnerabilities"]
+    if vulnerabliliy["file"] != "pom.xml":
+        #print (vulnerabliliy["file"], vulnerabliliy["severity"])
+        vuln += 1
+    else:
+        sca += 1
+print(vuln, sca)
